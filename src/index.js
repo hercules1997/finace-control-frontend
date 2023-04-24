9789f03fd2493bba25fe7d774332f8955878f78e
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-23 21:11:16 -0300

    aplicando rotas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GlobalStyles from "./styles/globalStyles";
+import AllRoutes from "./routes/routes";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-
-  </React.StrictMode>
-);
-
+  <>
+    <AllRoutes/>
+    <GlobalStyles />
+  </>
+)
