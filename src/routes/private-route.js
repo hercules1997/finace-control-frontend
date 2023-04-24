7e5f8f522aa35d2257149a27a6bd0cbf18224d1e
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-23 21:15:20 -0300

    modelo de rotas privadas

---
+++ src/routes/private-route.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+import { Header } from '../components'
+
+export function PrivateRoute ({ component, isAdmin, ...rest }) {
+  const user = localStorage.getItem('topBurguer:userData')
+
+  if (!user) {
+    return <Redirect to="/login" />
+  }
+
+  if (isAdmin && !JSON.parse(user).admin) {
+    return <Redirect to="/" />
+  }
+
+  return (
+    <>
+      {!isAdmin && <Header />}
+      <Route {...rest} component={component} />
+    </>
+  )
+}
+
+PrivateRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+  isAdmin: PropTypes.bool
+}
