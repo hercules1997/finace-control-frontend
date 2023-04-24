fff55f4ad7f2dc914642e0d3b20f19cb8a30cfbb
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-04-23 21:11:53 -0300

    adicionando imagem de fundo e criando o componente

---
+++ src/components/Background/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import background from '../../assets/backgroud.png'
+
+import { Logo } from './style'
+
+export function Background () {
+  return <Logo src={background} />
+}
+
---
+++ src/components/Background/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Logo = styled.img`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: -1000;
+  
+`
+
